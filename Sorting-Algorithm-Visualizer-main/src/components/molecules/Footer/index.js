5c1980ff27198e965e48ed3e1377e4a1daecd9b8
Lakshay Bhatia <97439907+lakshayEXE@users.noreply.github.com>
2024-07-02 09:32:51 +0530

    Add files via upload

---
+++ Sorting-Algorithm-Visualizer-main/src/components/molecules/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+const Footer = (props) => {
+  return (
+    <footer className="Footer">
+      {}
+    </footer>
+  );
+};
+
+export default Footer;
